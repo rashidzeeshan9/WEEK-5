2e3ea08174c3540bb5378c89659587149e10960e
rashidzeeshan9 <athan2505@outlook.com>
2024-11-22 21:35:30 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int size;
+	cout<<"Enter the size of array: ";
+	cin>>size;
+	int num[100];
+	
+	for(int i=0; i<size; i++){
+		
+		cin>>num[i];
+	}
+	
+	for(int i=0; i<size; i++){
+		
+		cout<<num[i]<<" ";
+	}
+	
+}
+
+
+
+
+
+
+
+
+
---
+++ q3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int array[]={2,5,6,9,8,9,6,3,1,4,5,7,5,8,9,6,8,6,5,4,47};
+	
+	
+	
+	int sizeofArray=sizeof(array)/sizeof(int);
+	
+	cout<<"Size of Array: "<<sizeofArray;
+	
+	
+	
+	
+	
+}
+
+
+
